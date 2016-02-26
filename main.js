65e90a71159ddc1803a1ffe0ede931856e911266
“Katy” <k.mcconica@gmail.com>
2016-02-26 08:29:54 -0800

    shuffle feature works and used for loops to create card objects. starting code to build cards using html css and svg img files

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var cardList = new Array();
 var specialCards = ["Jack", "Queen", "King", "Ace"];
 var suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
-
+var joker = "Joker";
 /* ---------- OBJECT PROTOTYPES ---------- */
 var Card = function(s, v) {
 	this.suit = s;
@@ -19,11 +19,89 @@ function shuffle(array) {
   }
   return array;
 }
-
+// function cardStructure(v, s) {
+// 	var div = document.createElement("div");
+// 	div.style.width = "100px";
+// 	div.style.height = "150px";
+// 	div.style.background = "white";
+// 	div.style.color = "black";
+// 	div.style.border = "solid black 1px";
+// 	div.style.float = "left";
+// 	document.body.appendChild(div);
+// }
+//checks object value
+// function strNumCheck(obj) {
+// 	console.log(obj)
+// 	var objectValue = obj.value;
+//  	if (Number.isInteger(objectValue) === true) {
+// 		var number = objectValue;
+// 		return number;
+// 	}
+// 	if (jQuery.type(objectValue) === 'string') {
+// 		var string = objectValue;
+// 		return string;
+// 	}
+// }
+//builds cards
 function buildCard(obj) {
-	return("<div class=''></div>")
+	var div = document.createElement("div");
+	div.style.width = "100px";
+	div.style.height = "150px";
+	div.style.background = "white";
+	div.style.color = "black";
+	div.style.border = "solid black 1px";
+	div.style.float = "left";
+		if (obj.suit === 'Hearts') {
+			if (jQuery.type(obj.value) === 'string') {
+				//create special card
+				console.log('special card')
+				document.body.appendChild(div);
+			} else {
+				console.log('number card')
+				document.body.appendChild(div);
+				div.innerHTML = "<p>" + obj.value + "</p><div class='inner-div'></div>";
+				$('.inner-div').css({'width':'70px','height':'120px'});
+			}
+		}
+		if (obj.suit ==='Clubs') {
+			if (jQuery.type(obj.value) === 'string') {
+				//create special card
+				console.log('special card')
+				document.body.appendChild(div);
+			} else {
+				console.log('number card')
+				document.body.appendChild(div);
+				div.innerHTML = "<p>" + obj.value + "</p><div class='inner-div'></div>";
+				$('.inner-div').css({'width':'70px','height':'120px'});
+			}
+		}
+		if (obj.suit === 'Diamonds') {
+				if (jQuery.type(obj.value) === 'string') {
+				//create special card
+				console.log('special card')
+				document.body.appendChild(div);
+			} else {
+				console.log('number card')
+				document.body.appendChild(div);
+				div.innerHTML = "<p>" + obj.value + "</p><div class='inner-div'></div>";
+				$('.inner-div').css({'width':'70px','height':'120px'});
+				$('p').css('width', '10px')
+			}
+		}
+		if ( obj.suit === 'Spades') {
+				if (jQuery.type(obj.value) === 'string') {
+				//create special card
+				console.log('special card')
+				document.body.appendChild(div);
+			} else {
+				console.log('number card')
+				document.body.appendChild(div);
+				div.innerHTML = "<p>" + obj.value + "</p><div class='inner-div'></div>";
+				$('.inner-div').css({'width':'70px','height':'120px'});
+			}
+		}
+		return("<div class=''></div>")
 }
-
 /* ---------- INSTANTIATE OBJECTS ----------- */
 // Standard card loop
 for(i=2; i<=10; i++) {
@@ -39,14 +117,20 @@ for(i=0; i<specialCards.length; i++) {
 		cardList.push( x );
 	}
 }
-
+// Joker card loop
+for(i=0; i<=2; i++) {
+		var x = new Card(joker);
+		cardList.push( x );
+}
+console.log(cardList)
 /* ---------- DOCUMENT READY ----------- */
 $(document).ready(function() {
 	$('button').on('click', function() {
 		shuffle(cardList);
 		$('#card-list').empty();
 		for(i=0; i<=50; i++) {
-			$('#card-list').append( buildCard(cardList[i]) );
+			var val = buildCard(cardList[i]);
+			$('#card-list').append( val );
 			console.log(cardList[i].value + ' of ' + cardList[i].suit + '<br />');
 		}	
 	});
